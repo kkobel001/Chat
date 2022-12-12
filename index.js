f0e6a2b442ceea5ac5ae5d805d2a229b3eb7ad63
Katarzyna Kobel <katarzyna.kobel@vp.pl>
2022-12-12 21:16:36 +0100

    Add sockiet.io

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express= require("express");
 const app= express();
 
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
 app.get('/', (req,res)=>{
     console.log(req);
-    res.send('Serwer działa')
+    res.send('<h1>Serwer działa</h1>')
 })
 
 app.listen(80,()=>{
